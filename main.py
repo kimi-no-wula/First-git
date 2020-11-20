0d1961281b6d54faed2582d06e6300903fe263b8
wula <61949840+kimi-no-wula@users.noreply.github.com>
2020-11-20 20:03:42 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,14 @@
+def main():
+    print('My first git program')
+    print('And I change it every day')
+    print('oh')
+    print('oh')
+    print('oh')
+    print('oh')
+    print('oh')
+    print('oh')
+    print('oh')
+    print('New day — new print')
+
+if __name__ == '__main__':
+    main()
